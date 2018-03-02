db7e9f49d66c40b1d10f2e948faf77df515aba92
Daniel Jünger <djuenger@students.uni-mainz.de>
2018-03-02 02:38:23 +0100

    rolling commit: I'm done with this shit for today.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 pip3 install --upgrade -r requirements.txt
+activate-global-python-argcomplete
 cp dockersh /usr/local/bin
 chmod +x /usr/local/bin/dockersh
 cp dockersh.ini /etc
+echo "done!"
