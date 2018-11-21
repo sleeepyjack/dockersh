abe526a65f8583697852b04821c033f564bec112
Sebastian Brodehl <brodehl@uni-mainz.de>
2018-11-21 16:06:41 +0100

    Add dest argument to install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 pip3 install --upgrade -r requirements.txt
-activate-global-python-argcomplete
+if [ -z "$1" ]; then
+    activate-global-python-argcomplete
+else
+    activate-global-python-argcomplete --dest=$1
+fi
 cp dockersh /usr/local/bin
 chmod +x /usr/local/bin/dockersh
 cp -n dockersh.ini /etc
